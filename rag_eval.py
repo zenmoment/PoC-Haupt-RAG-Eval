f6bad2773e1964414bcfb8fbdab481782a979f76
zenmoment <52459317+zenmoment@users.noreply.github.com>
2024-06-01 14:01:05 +0200

    Update rag_eval.py

--- rag_eval.py
+++ rag_eval.py
@@ -11,17 +11,14 @@ from ragas.llms import LangchainLLMWrapper
 
 load_dotenv()
 
-# Überprüfen, ob der Dateiname als Argument übergeben wurde
 if len(sys.argv) < 2:
     print("Bitte geben Sie den Namen der Eingabedatei als Argument an.")
     sys.exit(1)
 
 input_file = sys.argv[1]
 
-# questions_input.xlsx laden
 df = pd.read_excel(input_file)
 
-# Spalten überprüfen
 required_columns = ["question", "answer", "ground_truth", "contexts", "Buch"]
 if not all(col in df.columns for col in required_columns):
     raise ValueError(f"Die Eingabedatei muss die Spalten {', '.join(required_columns)} enthalten.")
@@ -61,7 +58,6 @@ azure_model = AzureChatOpenAI(
 
 ragas_azure_model = LangchainLLMWrapper(azure_model)
 
-# init the embeddings for answer_relevancy, answer_correctness and answer_similarity
 azure_embeddings = AzureOpenAIEmbeddings(
     openai_api_version=azure_configs["openai_api_version"],
     azure_endpoint=os.getenv("AZURE_OPENAI_BASE_URL"),
@@ -69,14 +65,11 @@ azure_embeddings = AzureOpenAIEmbeddings(
     model=azure_configs["embedding_name"],
 )
 
-# Metrics to use:
 metrics = [faithfulness, answer_relevancy, context_recall, context_precision]
 
-# Update metric <answer_relevancy> to Azure:
 answer_relevancy.llm = ragas_azure_model
 answer_relevancy.embeddings = azure_embeddings
 
-# Update all metrics to ragas Azure model:
 for m in metrics:
     m.__setattr__("llm", ragas_azure_model)
 
@@ -86,19 +79,15 @@ for i in range(len(ds)):
     result = evaluate(subset, metrics=metrics)
     print(result, "index", i)
     
-    # Ergebnisse in ein Dictionary umwandeln
     result_dict = {metric: result[metric] for metric in ['faithfulness', 'answer_relevancy', 'context_recall', 'context_precision']}
     results.append(result_dict)
 
-# Ergebnisse in ragas_result.xlsx speichern
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
 input_file_name = os.path.splitext(input_file)[0]
 output_file = f"{input_file_name}_results_{timestamp}_eval_gpt4.xlsx"
 
-print("-------------", results)
 result_df = pd.DataFrame(results, columns=['faithfulness', 'answer_relevancy', 'context_recall', 'context_precision'])
 
-# Ergebnisse mit ursprünglichem DataFrame verbinden
 result_df = pd.concat([df, result_df], axis=1)
 
 result_df.to_excel(output_file, index=False)
